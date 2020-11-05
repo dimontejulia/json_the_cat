1109ea5d7c72f986b020aa89456da671956f6ca9
Julia Di Monte <dimontejulia@gmail.com>
2020-11-05 19:39:05 +0000

    adding edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,24 @@
 const request = require("request");
+const args = process.argv;
+let catType = args.slice(2, args.length).join();
+const url = "https://api.thecatapi.com/v1/breeds/search?q=" + catType;
 
-request(
-  "https://api.thecatapi.com/v1/breeds/search?q=sibe",
-  (error, response, body) => {
+const findCat = (url) => {
+  request(url, (error, response, body) => {
+    if (error) {
+      throw new Error(error.message);
+    }
     const data = JSON.parse(body);
-    console.log(data);
-  }
-);
+    if (data.length === 0) {
+      console.log("Cat not found :(");
+    } else {
+      //cat exists
+      console.log(data);
+    }
+  });
+};
+
+findCat(url);
 
 //body is a string type
 //data is an object time
